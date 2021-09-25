efb4f2ed417869e23444708c3cccf172ad9bbc3c
Jackyho <79627347+HoHongPan@users.noreply.github.com>
2021-09-25 13:22:03 +0800

    Add files via upload

---
+++ codeitsuisse/routes/tic_tac_toe.py
@@ -0,0 +1,20 @@
+import logging
+import json
+import requests
+
+
+from flask import request, jsonify
+
+from codeitsuisse import app
+
+logger = logging.getLogger(__name__)
+
+@app.route('/tic-tac-toe', methods=['POST'])
+def tic_tac_toe():
+    data = request.get_json()
+    logging.info("data sent for evaluation {}".format(data))
+    id = data.get("battleId")
+    URL = "https://cis2021-arena.herokuapp.com/tic-tac-toe/start/" + id
+    table = [[0,0,0],[0,0,0],[0,0,0]]
+    r = requests.get(URL)
+    return json.dumps(URL)
