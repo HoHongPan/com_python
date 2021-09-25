114bc8b29c2d6ff2e3ef4103e7a8cc8b5a7c50c7
Jackyho <79627347+HoHongPan@users.noreply.github.com>
2021-09-25 13:34:32 +0800

    Update tic_tac_toe.py

--- codeitsuisse/routes/tic_tac_toe.py
+++ codeitsuisse/routes/tic_tac_toe.py
@@ -2,20 +2,20 @@ import logging
 import json
 import requests
 
-
 from flask import request, jsonify
 
 from codeitsuisse import app
 
 logger = logging.getLogger(__name__)
 
+
 @app.route('/tic-tac-toe', methods=['POST'])
 def tic_tac_toe():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
     id = data.get("battleId")
-    URL = "https://cis2021-arena.herokuapp.com/tic-tac-toe/start/" + id
-    table = [[0,0,0],[0,0,0],[0,0,0]]
+    url = "https://cis2021-arena.herokuapp.com/tic-tac-toe/start/" + id
+    table = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
     r = requests.get(URL)
     print(r)
-    return json.dumps(URL)
+    return json.dumps(url)
