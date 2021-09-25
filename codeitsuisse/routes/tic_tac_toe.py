a90a5f76ff8976bea7d85aba27ab6a61b8add407
Jackyho <79627347+HoHongPan@users.noreply.github.com>
2021-09-25 15:09:26 +0800

    Update tic_tac_toe.py

--- codeitsuisse/routes/tic_tac_toe.py
+++ codeitsuisse/routes/tic_tac_toe.py
@@ -18,6 +18,6 @@ def tic_tac_toe():
     url = 'https://cis2021-arena.herokuapp.com/tic-tac-toe/start/' + id
     table = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
     print(url)
-    r = urllib.request.urlopen(url, timeout= 0.0001)
+    r = urllib.request.urlopen(url)
     print(r.read().decode('utf-8'))
     return json.dumps(url)
